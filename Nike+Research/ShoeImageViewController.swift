67dce612f0cb5dcd76fd2e245c4edbb6ed5d7c68
Faiq Ali <faiqueali017@gmail.com>
2020-06-24 09:27:05 -0700

    Now till here if you run so you can see the images in your PageVC

--- Nike+Research/ShoeImageViewController.swift
+++ Nike+Research/ShoeImageViewController.swift
@@ -4,7 +4,11 @@ class ShoeImageViewController : UIViewController
 {
     @IBOutlet weak var imageView: UIImageView!
     
-    var image: UIImage?
+    var image: UIImage? {
+        didSet {
+            self.imageView?.image = image
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- Nike+Research/ShoeImagesPageViewController.swift
+++ Nike+Research/ShoeImagesPageViewController.swift
@@ -1,12 +1,103 @@
 import UIKit
 
 class ShoeImagesPageViewController: UIPageViewController {
+    
+    var images: [UIImage]? = Shoe.fetchShoes().first!.images
+    
+    struct Storyboard {
+        static let shoeImageViewController = "ShoeImageViewController"
+    }
+    
+    lazy var controllers: [UIViewController] = {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        var controllers = [UIViewController]()
+        if let images = self.images {
+            for image in images {
+                let shoeImageVC = storyboard.instantiateViewController(withIdentifier: Storyboard.shoeImageViewController)
+                controllers.append((shoeImageVC))
+            }
+        }
+        return controllers
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        automaticallyAdjustsScrollViewInsets = false
+        dataSource = self
+        delegate = self
+        
+        self.turnToPage(index: 0)
+        
     }
     
+    //MARK:- Helper Methods
+    
+    func turnToPage(index: Int) {
+        let controller = controllers[index]
+        var direction = UIPageViewControllerNavigationDirection.forward
+        
+        if let currentVC = viewControllers?.first {
+            let currentIndex = controllers.index(of: currentVC)!
+            if currentIndex > index {
+                direction = .reverse
+            }
+        }
+        
+        self.configureDisplaying(viewController: controller)
+        
+        setViewControllers([controller], direction: direction, animated: true, completion: nil)
+    }
+    
+    func configureDisplaying(viewController: UIViewController) {
+        for (index, vc) in controllers.enumerated() {
+            if viewController === vc {
+                if let shoeImageVC = viewController as? ShoeImageViewController {
+                    shoeImageVC.image = self.images?[index]
+                    
+                }
+            }
+        }
+    }
 
+}
+
+// MARK:- UIPageViewControllerDataSource
+extension ShoeImagesPageViewController : UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.index(of: viewController) {
+            if index > 0 {
+                return controllers[index-1]
+            }
+        }
+        
+        return controllers.last
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let index = controllers.index(of: viewController) {
+            if index < controllers.count - 1 {
+                return controllers[index + 1]
+            }
+        }
+        
+        return controllers.first
+    }
+    
+}
+
+// MARK:- UIPageViewControllerDelegate
+extension ShoeImagesPageViewController : UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        self.configureDisplaying(viewController: pendingViewControllers.first as! ShoeImageViewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if !completed {
+            self.configureDisplaying(viewController: previousViewControllers.first as! ShoeImageViewController)
+        }
+    }
+    
 }
